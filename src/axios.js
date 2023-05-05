aa3ffaf3fed17990fb69c71c8fc4513445e72c0c
Vlad Kost <kostvkthekost@gmail.com>
2023-05-05 14:29:18 +0300

    init

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://react2023-backend.vercel.app/" // "http://localhost:4444" ,
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4444" ,
 });
 
 instance.interceptors.request.use((config) => {
