2010bb0d968df1093b9894636d8f87a0e21d3782
TheKost_ <thekost2@yandex.ru>
2023-05-07 23:21:59 +0300

    rework userInfo

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import styles from './UserInfo.module.scss';
 
-export const UserInfo = ({ avatarUrl, userName, additionalText }) => {
-  return (
-    <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || "/ui/profile/noavatar.png"} alt={userName} />
-      <div className={styles.userDetails}>
-        <span className={styles.userName}>{userName}</span>
-        <span className={styles.additional}>{additionalText}</span>
-      </div>
-    </div>
-  );
+export const UserInfo = ({avatarUrl, userName, additionalText}) => {
+
+    return (
+        <>
+            <div className={"p-4"}>
+                <div className={"flex justify-content-between"}>
+                    <div className={"flex"}>
+                        <div className={"mx-2 flex align-items-center"}>
+                            <div className="w-12 h-12 rounded-full overflow-hidden flex-shrink-0">
+                                <img
+                                    className="object-cover w-full h-full"
+                                    src={avatarUrl || "/ui/profile/noAvatar.png"}
+                                    alt="User avatar"
+                                />
+                            </div>
+                        </div>
+
+                        <div className={"pl-2"}>
+
+                            <div className={"text-blue-900 hover:underline"}>
+
+                                {userName}
+
+                            </div>
+
+                            <div className={"text-gray-500 hover:underline"}>
+
+                                {additionalText}
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
